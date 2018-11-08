6fa3cd4bbf9f1a21a34fce397b9524baba736ee8
AndresBolanos <ronald-9797@hotmail.com>
2018-11-08 12:59:18 -0600

    Add files via upload

---
+++ Generador-Artificial/JsonCreation.js
@@ -0,0 +1,71 @@
+//Funcion para crear el json a partir de un arreglo
+var estructura = [];
+var jsonObj = [];
+var finalJson = [];
+var vistos = [];
+function createJson(arreglo){
+	estructura = arreglo;
+	createJsonAux(arreglo);
+  	var jsonObj = JSON.stringify(finalJson);
+  	var final = "";
+  	for (var i = 0; i < jsonObj.length; i++){
+  		if (i > 0 && i < jsonObj.length-1){
+  			if (jsonObj[i] == ","){
+  				final = final + jsonObj[i];
+  				final = final + "\n";
+  			}
+  			else{
+  				final = final + jsonObj[i];
+  			}
+  			
+  		}
+  	}
+	var blob = new Blob([final], {type: "text/plain;charset=utf-8"});
+  	saveAs(blob, "NewJson"+".json");
+}
+
+function createJsonAux(arreglo){
+	var json = [];
+	//Creamos el nodo raiz
+	for (var i = 0; i < arreglo.length; i++){
+		if (isView(arreglo[i].name) == false){
+			vistos.push(arreglo[i].name);
+			var item = {};
+			item["name"] = arreglo[i].name;
+			item["Synonym"] = arreglo[i].Synonym;
+			item["author"] = arreglo[i].author;
+			item["record_scrutiny_date"] = arreglo[i].record_scrutiny_date;
+			if ((SelectParents(arreglo[i].name).length) > 0){
+				item["children"] = createJsonAux(SelectParents(arreglo[i].name));
+			}
+			json.push(item);
+		}
+	}
+	var jsonObj = JSON.stringify(json);
+	console.log(jsonObj);
+  	
+  	finalJson = json;
+	return json;
+}
+
+function SelectParents(name){
+	var hijos = [];
+	for (var i = 0; i < estructura.length; i++){
+		if (estructura[i].parent != undefined){
+			if (estructura[i].parent.name == name){
+				var nuevo = $.extend( {}, estructura[i]);
+				hijos.push(nuevo);
+			}
+		}
+	}
+	return hijos;
+}
+
+function isView(name){
+	for (var i = 0; i < vistos.length; i++){
+		if (vistos[i] == name){
+			return true;
+		}
+	}
+	return false;
+}
